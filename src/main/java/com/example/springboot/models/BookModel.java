d0dda484a782b1a7822806f486545af419d737f5
Kastet6398 <konstantin.nezhalsky@gmail.com>
2024-01-30 18:03:06 +0200

    3

--- src/main/java/com/example/springboot/models/BookModel.java
+++ src/main/java/com/example/springboot/models/BookModel.java
@@ -2,10 +2,10 @@ package com.example.springboot.models;
 
 import java.util.ArrayList;
 
-public record BookModel(String name, String urlOfContent, String author, String coverSheet, ArrayList<Integer> categoryId, int releaseYear, int pagesAmount, int id) implements SingleObjectModel {
+public record BookModel(String name, String urlOfContent, String author, String coverSheet, ArrayList<Integer> categoryId, int releaseYear, int pagesAmount, int id, int genre) implements SingleObjectModel {
     @SuppressWarnings("unused")
-    public BookModel(String name, String urlOfContent, String author, String coverSheet, ArrayList<Integer> categoryId, int pagesAmount, int releaseYear) {
-        this(name, urlOfContent, author, coverSheet, categoryId, pagesAmount, releaseYear, -1);
+    public BookModel(String name, String urlOfContent, String author, String coverSheet, ArrayList<Integer> categoryId, int pagesAmount, int releaseYear, int genre) {
+        this(name, urlOfContent, author, coverSheet, categoryId, pagesAmount, releaseYear, genre, -1);
     }
     @Override
     public int id() {
