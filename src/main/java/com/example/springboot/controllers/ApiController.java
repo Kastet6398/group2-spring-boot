5637862cf143316a43bae29305f2325dcf54d64d
Kastet6398 <konstantin.nezhalsky@gmail.com>
2024-01-30 09:34:02 +0200

    9

--- src/main/java/com/example/springboot/controllers/ApiController.java
+++ src/main/java/com/example/springboot/controllers/ApiController.java
@@ -1,14 +1,15 @@
 package com.example.springboot.controllers;
 
 import com.example.springboot.models.*;
+import com.example.springboot.utils.Constants;
 import com.example.springboot.utils.Utils;
-import org.springframework.web.bind.annotation.*;
-
-
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -20,7 +21,7 @@ public class ApiController {
         String greeting = "Hello";
 
         if (user != null) {
-            greeting += STR.", \{user.firstName()} \{user.lastName()}";
+            greeting += STR.", " + user.firstName() + " " + user.lastName();
         }
 
         greeting += "!";
@@ -31,13 +32,13 @@ public class ApiController {
     @PostMapping("/sign-up")
     public BaseModel signUp(HttpServletResponse response, @RequestBody UserModel user) throws IOException {
         String token = Utils.register(user);
+
         if (token == null) {
             return new SuccessModel(false, "registration failed");
         }
+
         Cookie cookie = new Cookie("token", token);
-        cookie.setPath("/");
-        cookie.setMaxAge(100000);
-        response.addCookie(cookie);
+        configureCookie(response, cookie);
 
         return new SuccessModel(true, "registered successfully");
     }
@@ -45,15 +46,15 @@ public class ApiController {
     @PostMapping("/create-book")
     public BaseModel createBook(@CookieValue(name = "token", defaultValue = "") String token, @RequestBody BookModel book) {
         UserModel user = Utils.getUser(token);
+
         if (user == null) {
             return new SuccessModel(false, "please login first");
         }
+
         try {
             if (Utils.createBook(book)) {
-
                 return new SuccessModel(true, "created successfully");
-            }
-            else {
+            } else {
                 return new SuccessModel(false, "creation failed");
             }
         } catch (Exception e) {
@@ -61,18 +62,34 @@ public class ApiController {
         }
     }
 
+    @PostMapping("/list-books")
+    public BookTableModel listBooks(@RequestParam(name = "categories", required = false) ArrayList<Integer> categories, @RequestParam(name = "search", required = false) String search) throws IOException {
+        List<BookModel> filteredBooks = ((BookTableModel) Utils.readJson(Constants.BOOK_TABLE_FILE, BookTableModel.class)).books();
+        if (categories != null && !categories.isEmpty()) {
+            filteredBooks = filteredBooks.stream()
+                    .filter(x -> x.categoryId().stream().anyMatch(categories::contains))
+                    .toList();
+        }
+
+        if (search != null && !search.isEmpty()) {
+            filteredBooks = filteredBooks.stream().filter(x -> x.name().contains(search)).toList();
+        }
+
+        return new BookTableModel(new ArrayList<>(filteredBooks));
+    }
+
     @PostMapping("/create-category")
     public BaseModel createBookCategory(@CookieValue(name = "token", defaultValue = "") String token, @RequestBody BookCategoryModel category) {
         UserModel user = Utils.getUser(token);
+
         if (user == null) {
             return new SuccessModel(false, "please login first");
         }
+
         try {
             if (Utils.createBookCategory(category)) {
-
                 return new SuccessModel(true, "created successfully");
-            }
-            else {
+            } else {
                 return new SuccessModel(false, "creation failed");
             }
         } catch (Exception e) {
@@ -84,13 +101,13 @@ public class ApiController {
     public BaseModel login(HttpServletResponse response, @RequestBody LoginUserModel user) throws IOException {
         System.out.println(user);
         String token = Utils.login(user);
+
         if (token == null) {
             return new SuccessModel(false, "logging in failed");
         }
+
         Cookie cookie = new Cookie("token", token);
-        cookie.setPath("/");
-        cookie.setMaxAge(100000);
-        response.addCookie(cookie);
+        configureCookie(response, cookie);
 
         return new SuccessModel(true, "logged in successfully");
     }
@@ -104,5 +121,11 @@ public class ApiController {
         response.addCookie(cookie);
 
         return new SuccessModel(true, "logged out successfully");
+    }
+
+    private void configureCookie(HttpServletResponse response, Cookie cookie) {
+        cookie.setPath("/");
+        cookie.setMaxAge(100000);
+        response.addCookie(cookie);
     }
 }
